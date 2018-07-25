37e2677b8ae99664db87aac0a0b3c328dc49e89c
Heds Simons <heds@whaleway.net>
2018-07-25 10:08:49 +0100

    All commits

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 console.log('Output shows I live!');
 console.log('And now with extra output!');
-console.log('This will build on circle');
-console.log('This will not build on circle');
+console.log('This will build on circle as a PR and then as a merge');
